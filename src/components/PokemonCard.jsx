86176576de9620e3a7dd1694b5f39451d81dac7c
Rafael Siqueira Leonardo <rafaelsiqueiraleonardo@gmail.com>
2025-12-05 22:21:44 -0300

    asdasdasdas
    asdasadsa

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,17 @@
 export default function PokemonCard({pokemon}){
     return (
-        <div>
+        <div style={{
+            background: "#000000",
+            padding: "20px",
+            width: "300px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
+            border: "1px solid #ddd"
+        }}>
         <img src={pokemon.imgSrc} height={200}/>     
         <label style={{marginRight:5}} >{pokemon.name}</label>
         </div>
+
+        
     )
 }
--- src/components/PokemonConteiner.jsx
+++ src/components/PokemonConteiner.jsx
@@ -27,7 +27,8 @@ export default function PokemonConteiner({ type }) {
         <div style={{
            
             display:"grid",
-            gridTemplateColumns: "1fr 1fr 1fr"
+            gridTemplateColumns: "1fr 1fr 1fr",
+            justifyContent: "space-around"
         }}>
             {
                 pokemons.map((pokemonCorrenteDoFor, index) => {
